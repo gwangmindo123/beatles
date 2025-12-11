2d2f8ac0e70396bc7edc148e6f831e887a29cf98
도광민 <as1545915@hs.ac.kr>
2025-12-11 15:45:19 +0000

    Feat: Restore fullpage.js, remove debug CSS, and enhance .cont_0 aesthetics

--- js/fullpage_initial.js
+++ js/fullpage_initial.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  /*
   $('#fullpage').fullpage({
     //options here
     licenseKey:'OPEN-SOURCE-GPLV3-LICENSE',
@@ -10,5 +9,4 @@ $(document).ready(function() {
     fixedElements: 'header',
     paddingTop: '100px' // Add padding to sections to account for the fixed header
   });
-  */ // Temporarily disable fullpage.js for debugging
 });
