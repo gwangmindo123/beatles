1de11ebf033afdef10ae1f1abe36b4dd0a88b74e
도광민 <as1545915@hs.ac.kr>
2025-12-11 15:39:25 +0000

    Debug: Add aggressive CSS borders/backgrounds and temporarily disable fullpage.js for visibility diagnosis

--- js/fullpage_initial.js
+++ js/fullpage_initial.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /*
   $('#fullpage').fullpage({
     //options here
     licenseKey:'OPEN-SOURCE-GPLV3-LICENSE',
@@ -9,4 +10,5 @@ $(document).ready(function() {
     fixedElements: 'header',
     paddingTop: '100px' // Add padding to sections to account for the fixed header
   });
+  */ // Temporarily disable fullpage.js for debugging
 });
