2566bead621fe2bb271910258cdce59bdd20e318
sgayoung129 <sgayoung129@gmail.com>
2025-12-12 18:47:11 +0000

    노래 추천 링크 오류 수정

--- js/recommend.js
+++ js/recommend.js
@@ -12,22 +12,22 @@ $(function(){
       {
         song: "Good Day Sunshine",
         album: "Revolver",
-        youtube: "https://www.youtube.com/embed/6xXw3cR14u0"
+        youtube: "https://youtu.be/R9ncBUcInTM?si=oPy4K1Atndj0ZB0Q"
       },
       {
         song: "Ob-La-Di, Ob-La-Da",
         album: "The Beatles (White Album)",
-        youtube: "https://www.youtube.com/embed/Qh4Y_j_d5tI"
+        youtube: "https://youtu.be/9x5WY_jmsko?si=zwztGyktFfSUHAxq"
       },
       {
         song: "Penny Lane",
         album: "Magical Mystery Tour",
-        youtube: "https://www.youtube.com/embed/S-rB0pHIFpQ"
+        youtube: "https://youtu.be/S-rB0pHI9fU?si=PgmAOc191JzO8rsp"
       },
       {
         song: "All You Need Is Love",
         album: "Magical Mystery Tour",
-        youtube: "https://www.youtube.com/embed/dsuxEaL_Nio"
+        youtube: "https://youtu.be/_7xMfIp-irg?si=RNNCV2BsaW0fx0ls"
       }
     ],
     sad: [
@@ -39,76 +39,76 @@ $(function(){
       {
         song: "Eleanor Rigby",
         album: "Revolver",
-        youtube: "https://www.youtube.com/embed/HuS5p4RjJ4s"
+        youtube: "https://youtu.be/HuS5NuXRb5Y?si=7SbBbu_Xjxsqxn6E"
       },
       {
         song: "The Long and Winding Road",
         album: "Let It Be",
-        youtube: "https://www.youtube.com/embed/fT6Q1z_o4lY"
+        youtube: "https://youtu.be/fR4HjTH_fTM?si=E-p84IVO_zoktusr"
       },
       {
         song: "Julia",
         album: "The Beatles (White Album)",
-        youtube: "https://www.youtube.com/embed/sIqP-k1y0EU"
+        youtube: "https://youtu.be/TZip_br_v3w?si=ZeRA58BeRcxxeRtc"
       },
       {
         song: "For No One",
         album: "Revolver",
-        youtube: "https://www.youtube.com/embed/2-nFh8E8Cq4"
+        youtube: "https://youtu.be/hgJ7dmr_ysU?si=hs2MR5Lq3kCpV7EN"
       }
     ],
     energetic: [
       {
         song: "Twist and Shout",
         album: "Please Please Me",
-        youtube: "https://www.youtube.com/embed/eFW2s1g3_9o"
+        youtube: "https://youtu.be/2RicaUqd9Hg?si=BHSvw8meu5u6ZSSU"
       },
       {
         song: "Help!",
         album: "Help!",
-        youtube: "https://www.youtube.com/embed/Qy3H7IeK52g"
+        youtube: "https://youtu.be/2Q_ZzBGPdqE?si=EEpkoCjITka9s7So"
       },
       {
         song: "Drive My Car",
         album: "Rubber Soul",
-        youtube: "https://www.youtube.com/embed/EZoY9s8sQ1M"
+        youtube: "https://youtu.be/6ZwNO_zAqOo?si=BKCWrADYqcraPtjM"
       },
       {
         song: "A Hard Day's Night",
         album: "A Hard Day's Night",
-        youtube: "https://www.youtube.com/embed/YpS8y18tT6Y"
+        youtube: "https://youtu.be/Yjyj8qnqkYI?si=0iIF52yO5XC5Mu_O"
       },
       {
         song: "Helter Skelter",
         album: "The Beatles (White Album)",
-        youtube: "https://www.youtube.com/embed/u_J6hX2o32E"
+        youtube: "https://youtu.be/vWW2SzoAXMo?si=iHPs75EH7FyzOtOJ"
       }
     ],
     calm: [
       {
         song: "Blackbird",
         album: "The Beatles (White Album)",
-        youtube: "https://www.youtube.com/embed/Man4Xw8Xypo"
+        youtube: "https://youtu.be/Man4Xw8Xypo?si=vjU-YX8YkI9VXHsU"
       },
       {
         song: "Across the Universe",
         album: "Let It Be",
-        youtube: "https://www.youtube.com/embed/90gpY1uE91s"
+        youtube: "https://youtu.be/90M60PzmxEE?si=nEoLzKMn8IDz2EbQ"
       },
       {
         song: "Here, There and Everywhere",
         album: "Revolver",
-        youtube: "https://www.youtube.com/embed/sK9kMhJ_0xY"
+        youtube: "https://youtu.be/KQetemT1sWc?si=QvNTy6D4XnVRYoFu"
       },
       {
         song: "Something",
         album: "Abbey Road",
-        youtube: "https://www.youtube.com/embed/Jl5gN31eKck"
+        youtube: "https://youtu.be/UelDrZ1aFeY?si=-F-dphHbw_vBcSTg"
       },
       {
         song: "In My Life",
         album: "Rubber Soul",
-        youtube: "https://www.youtube.com/embed/z9ypFjX-82g"
+        youtube: "https://youtu.be/mBqqeqcJM_0?si=Tbj2EEN8gSDcKAIe"
       }
     ]
   };
